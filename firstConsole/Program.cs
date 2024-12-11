2174fc701c9eb537dae4d310c162e96dc89b0c7a
Nathan Soucier <soucienp@gmail.com>
2024-12-11 16:36:09 -0500

    added password generator package

--- firstConsole/Program.cs
+++ firstConsole/Program.cs
@@ -1,7 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using PasswordGenerator;
+
 Console.WriteLine("What is your name?");
 var name = Console.ReadLine();
 var currentDate = DateTime.Now;
 Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate}!");
-Console.Write($"{Environment.NewLine}Press any key to exit...");
+Console.WriteLine($"{Environment.NewLine}Press any key to exit...");
 Console.ReadKey(true);
+
+// Tutorial using a package
+var pwd = new Password();
+var password = pwd.Next();
+Console.WriteLine(password.ToString());
